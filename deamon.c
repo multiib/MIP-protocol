6e3497c032542132c205fedf87ecca9e50fef2d6
Benjamin Borge <multiib@benjamins-mbp.lan>
2023-09-27 23:13:03 +0200

    Created empty files

---
+++ deamon.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/epoll.h>
+#include <linux/if_packet.h>
+#include <net/ethernet.h>
+#include <arpa/inet.h>
+
+#include "common.h"
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
