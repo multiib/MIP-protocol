45e1127ac673224f6881eac85d4a6a8696892e00
Pathujah Vimalasenan <pathujahvimalasenan@pathujahs-mbp-2.lan>
2023-10-11 01:27:06 +0200

    Worked

--- include/arp.h
+++ include/arp.h
@@ -7,14 +7,14 @@
 #define ARP_BROADCAST	{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}
 
 typedef struct {
-    uint8_t ip;
+    uint8_t mip;
     uint8_t mac[6];
 } ArpEntry;
 
-void print_arp_cache(ArpEntry *arp_cache);
+void print_arp_cache(ArpEntry *);
 void arp_init();
-uint8_t* arp_lookup(uint8_t ip);
-void arp_insert(uint8_t ip, uint8_t mac[6]);
-void arp_broadcast(uint8_t target_ip);
+uint8_t* arp_lookup(uint8_t);
+void arp_insert(uint8_t, uint8_t);
+void arp_broadcast(uint8_t);
 
 #endif // ARP_H
