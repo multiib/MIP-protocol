8a1120e62586666866a1e4d9950fbd487d5f1add
Pathujah Vimalasenan <pathujahvimalasenan@pathujahs-mbp-2.lan>
2023-10-16 22:26:15 +0200

    Worked

--- src/ping_server.c
+++ src/ping_server.c
@@ -67,7 +67,7 @@ int main(int argc, char *argv[]) {
         char *destination_host = "11"; // Filler value
         
         // Fill the buffer with the pong message
-        fill_pong_buf(buf, sizeof(buf), destination_host, message);
+        fill_pong_buf(buf, sizeof(buf), destination_host, strcpy(str, str + 5));
 
         // Write back
         rc = write(sd, buf, sizeof(buf));
