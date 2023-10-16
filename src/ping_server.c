0c112e5a3d971f5332604fba42c0dec9ef309dee
Pathujah Vimalasenan <pathujahvimalasenan@pathujahs-mbp-2.lan>
2023-10-16 23:32:42 +0200

    Worked

--- src/ping_server.c
+++ src/ping_server.c
@@ -54,15 +54,13 @@ int main(int argc, char *argv[]) {
             close(sd);
             exit(EXIT_FAILURE);
         }
-
-        printf("DEBUGp2\n");
-
+        printf("Received1: ");
+        printf("%d\n", read_buf[0]);
         char *str = uint32ArrayToString(read_buf);
         printf("%s\n", str);
 
-        printf("DEBUGp3\n");
-
-
+        printf("Received2: ");
+        printf("%d\n", read_buf[0]);
         char *destination_host = "11"; // Filler value
         
         // Fill the buffer with the pong message
