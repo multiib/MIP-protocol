d981b1e7724a3a66d4631713ff113525d9c5d9cb
Benjamin Borge <multiib@icloud.com>
2023-11-17 19:33:42 +0100

    add

--- include/route.h
+++ include/route.h
@@ -30,7 +30,11 @@ struct NeighborStatus {
     int isReachable;
 };
 
-
+extern int neighborTable[MAX_NODES];
+extern struct RoutingEntry routingTable[MAX_NODES];
+extern struct NeighborStatus neighborStatus[MAX_NODES];
+extern uint8_t localMIP;
+extern int routingTableHasChanged;
 
 void initializeRoutingTable(struct RoutingEntry *table, int size);
 
